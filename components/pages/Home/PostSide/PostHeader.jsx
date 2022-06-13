f0a1283ea5ca4ff04d1dfd2b4aaedfd003b03802
morshedulmunna <morshedulmunna1@gmail.com>
2022-06-13 10:19:40 +0600

    post area Design

--- components/pages/Home/PostSide/PostHeader.jsx
+++ components/pages/Home/PostSide/PostHeader.jsx
@@ -7,7 +7,7 @@ const PostHeader = () => {
     <>
       <Modal />
       {/* <label for="post-modal">open modal</label> */}
-      <div className="sticky top-0 flex gap-5 pt-2 pb-1 bg-white drop-shadow-lg">
+      <div className="sticky top-0 flex w-full gap-2 px-2 pt-2 pb-1 bg-white drop-shadow-lg">
         <img
           className="object-cover w-12 h-12 mb-3 rounded-full"
           src="https://i.ibb.co/k8SV0R9/postpic2.jpg"
@@ -15,7 +15,7 @@ const PostHeader = () => {
         />
         <form className="w-full">
           <textarea
-            class="textarea textarea-ghost placeholder:text-xl w-full resize-none text-xl mb-3"
+            class="textarea textarea-ghost placeholder:text-xl w-full resize-none text-xl mb-2 "
             placeholder="Whats on your Mind......?"
             rows="2"
             cols="50"
@@ -28,7 +28,12 @@ const PostHeader = () => {
               <PostInput />
               <PostInput />
             </div>
-            <button className="mb-2 btn-big">Post Now</button>
+            <button className="hidden mb-2 btn-big lg:block md:block">
+              Post Now
+            </button>
+            <button className="block mb-2 mr-2 btn-Sml lg:hidden md:hidden">
+              Post Now
+            </button>
           </div>
         </form>
       </div>
